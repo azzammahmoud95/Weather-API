1fe973c35e62f7798c713870f8e229fedc8d25a5
Azzam Mahmoud <azzammahmoud007@gmail.com>
2022-12-19 10:22:36 +0200

    Weather API: HTML/CSS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
-import Search from "./components/Search";
+//import Search from "./components/Search";
 
-import SayHi, { SayHello } from "./components/WeatherItem";
+//import SayHi, { SayHello } from "./components/WeatherItem";
 import fakeWeatherData from "./fakeWeatherData.json";
-
+//import MyName from "./components/MyName";
+import clear from "./img/weather-icons/clear.svg";
+import cloudy from "./img/weather-icons/partlycloudy.svg";
 import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "Karim"
+      name: "London"
     };
   }
 
@@ -21,10 +23,55 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
-        <h1>Azzam Mahmoud</h1>
+        <form className="app__header" >
+            <input type="text" placeholder="Type City Name" className="search__input"></input>
+            <button className="btn-find-weather">FIND WEATHER</button>
+        </form>
+        <div className="app__main" >
+        <div className="weather__now">
+        <img src={cloudy} className="img__now" />
+            <p className="subHeader">Overcast clouds</p>
+            <p><span>Temprature</span> 10° to 11°C</p>
+            <p><span>Humidity</span> 78% <span>pressure</span> 1008.48</p>
+        </div>
+        <div className="weather__daily">
+        <div className="wheather__card">
+                    <p>03:00 </p>
+                    <img src={cloudy} />
+                    <p>8°C</p>
+              </div>
+              <div className="wheather__card">
+                    <p>06:00 </p>
+                    <img src={cloudy}  />
+                    <p>9°C</p>
+              </div>
+              <div className="wheather__card">
+                    <p>09:00 </p>
+                    <img src={clear}  />
+                    <p>14°C</p>
+              </div>
+              <div className="wheather__card">
+                    <p>12:00 </p>
+                    <img src={clear}  />
+                    <p>17°C</p>
+              </div>
+              <div className="wheather__card">
+                    <p>15:00 </p>
+                    <img src={clear} />
+                    <p>18°C</p>
+              </div>
+              <div className="wheather__card">
+                    <p>18:00 </p>
+                    <img src={clear}  />
+                    <p>16°C</p>
+              </div>
+              <div className="wheather__card">
+                    <p>21:00 </p>
+                    <img src={cloudy}  />
+                    <p>13°C</p>
+              </div>
+        </div>
+        </div>
       </div>
     );
   }
